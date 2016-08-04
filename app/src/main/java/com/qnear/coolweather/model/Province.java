0a48259f83971fe2fe6adfdbbd64ec50a40eeffe
lean <lean@gmail.com>
2016-08-04 18:18:11 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/qnear/coolweather/model/Province.java
@@ -0,0 +1,38 @@
+package com.qnear.coolweather.model;
+
+/**
+ * Created by 陈振聪 on 2016/8/4.
+ */
+public class Province {
+    private int id ;
+    private String provinceName ;
+    private String provinceCode ;
+
+
+    public int getId()
+    {
+        return id ;
+    }
+    public void setId(int id)
+    {
+        this.id = id ;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+}
